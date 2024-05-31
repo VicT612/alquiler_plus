d7f3b66ca74d1c1e11072f416e6a172f83414a03
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-05-31 10:45:06 -0400

    tailwind add

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider} from "next-auth/react";
 import '../app/globals.css'
+import 'tailwindcss/tailwind.css'
 export interface AuthContextProps {
   children: React.ReactNode;
 }
